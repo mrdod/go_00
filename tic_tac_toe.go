185c0c092d1fd113fe2181258753ea1a4a22a722
Mr. Dod <chetdodge@gmail.com>
2020-05-15 11:42:29 -0400

    Updated to use workspace

--- tic_tac_toe.go
+++ tic_tac_toe.go
@@ -1,32 +1,65 @@
 package main
 
-import(
+import (
 	"bufio"
 	"os"
 	"strings"
 )
-func Init( ttt_buff []string ) int{
-	// Init
-	for ii:= 1; ii < 9; ii++ {
-		ttt_buff[ii] = " "
-	}
 
-	return 0;
+type workspace struct {
+	ttt_buff [9]string
+	cursor_pos int
+	turn int
+	game_fin bool
+	text string
+	reader *bufio.Reader
 }
 
-func Build_Output( ttt_buff []string, cursor_pos int ){
+
+func main() {
+	work := workspace{
+		cursor_pos: 0,
+		turn:       0,
+		game_fin:	false,
+		reader:		nil,
+	}
+
+	work.reader = bufio.NewReader(os.Stdin)
+
+	Init( &work )
+	Build_Output( work )
+
+	for work.game_fin == false {
+		Read_Cmd( &work )
+		Proc_cmd( &work )
+
+		print(work.text)
+
+		Build_Output( work )
+	}
+}
+
+
+func Init( work *workspace ) {
+	// Init
+	for ii:= 1; ii < 9; ii++ {
+		work.ttt_buff[ii] = " "
+	}
+}
+
+func Build_Output( work workspace ){
 	kk := 0
 
 	for ii:= 0; ii < 3; ii++ {
 		for jj:= 0; jj < 3; jj++ {
 
 			// Print cursor
-			if cursor_pos == kk {
+			if work.cursor_pos == kk {
 				print("$")
 			}
 
 			// Print marker
-			print(ttt_buff[kk])
+			print(work.ttt_buff[kk])
 
 			// Print dividing line
 			if jj < 2 {
@@ -42,46 +75,31 @@ func Build_Output( ttt_buff []string, cursor_pos int ){
 	}
 }
 
-func Read_Cmd(reader *bufio.Reader) string {
+func Read_Cmd(work *workspace){
 
-	text, _ := reader.ReadString('\n')
-
-	return text
+	work.text, _ = work.reader.ReadString('\n')
 }
 
 
-func main() {
 
-	game_fin := false
-	ttt_buff := [9]string{" "}
 
-	reader := bufio.NewReader(os.Stdin)
-
-	cursor_pos := Init( ttt_buff[:] )
-	Build_Output( ttt_buff[:], cursor_pos )
-
-	for game_fin == false {
-		text := Read_Cmd( reader )
-		cursor_pos = Proc_cmd( text, ttt_buff[:], cursor_pos )
-		print(text)
-
-		if strings.Contains(text, "x") {
-
-		}
-		Build_Output( ttt_buff[:], cursor_pos )
+func Proc_cmd( work *workspace) {
+	if strings.Compare(work.text, "d\r\n") == 0 && ((work.cursor_pos + 1) % 3) != 0 {
+		work.cursor_pos++
+	} else if strings.Compare(work.text, "a\r\n") == 0 && (work.cursor_pos != 0 && work.cursor_pos != 3 && work.cursor_pos != 6) {
+		work.cursor_pos--
+	} else if strings.Compare(work.text, "s\r\n") == 0 && (work.cursor_pos != 6 && work.cursor_pos != 7 && work.cursor_pos != 8) {
+		work.cursor_pos = work.cursor_pos + 3;
+	} else if strings.Compare(work.text, "w\r\n") == 0 && (work.cursor_pos != 0 && work.cursor_pos != 1 && work.cursor_pos != 2) {
+		work.cursor_pos = work.cursor_pos - 3;
 	}
-}
 
-func Proc_cmd(text string, ttt_buff []string, cursor_pos int) int {
-	if strings.Compare(text, "d\r\n") == 0 && ((cursor_pos + 1) % 3) != 0 {
-		cursor_pos++
-	} else if strings.Compare(text, "a\r\n") == 0 && (cursor_pos != 0 && cursor_pos != 3 && cursor_pos != 6) {
-		cursor_pos--
-	} else if strings.Compare(text, "s\r\n") == 0 && (cursor_pos != 6 && cursor_pos != 7 && cursor_pos != 8) {
-		cursor_pos = cursor_pos + 3;
-	} else if strings.Compare(text, "w\r\n") == 0 && (cursor_pos != 0 && cursor_pos != 1 && cursor_pos != 2) {
-		cursor_pos = cursor_pos - 3;
+	if strings.Compare(work.text, "x\r\n") == 0 && strings.Compare(work.ttt_buff[work.cursor_pos], " ") == 0 {
+		work.ttt_buff[work.cursor_pos] = "x"
 	}
-	return( cursor_pos )
+
+	if strings.Compare(work.text, "o\r\n") == 0 && strings.Compare(work.ttt_buff[work.cursor_pos], " ") == 0 {
+		work.ttt_buff[work.cursor_pos] = "o"
+	}
 }
 
